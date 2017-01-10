e1673badc3e2832f18f5da469faa764c3c901b2a
Ekaterina <kat.skorupina@yandex.ru>
2017-01-10 22:32:35 +0000

    implemented a base logic of timestamp microservice

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var express = require('express')
+var timestamp = require("unix-timestamp")
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:date', function (req, res) {
+  var reqDate = req.params.date;
+  
+  //1. Пробуем конвертировать natural -> unix
+  var dateUnix = timestamp.fromDate(reqDate);
+  //2. Пробуем конвертировать в Date
+  var dateNatural = new Date(Number(reqDate)*1000);
+  
+  res.setHeader('Content-Type', 'application/json');
+
+  if (isNaN(dateUnix) && isNaN(dateNatural)) {
+      res.send(JSON.stringify({ unix: null, natural : null}));
+    } 
+  else if (isNaN(dateUnix)){
+      dateNatural = dateNatural.getMonth() + " " + dateNatural.getDay() + ", " + dateNatural.getFullYear(); 
+      res.send(JSON.stringify({ unix: reqDate, natural : dateNatural}));
+  } else {
+    res.send(JSON.stringify({ unix: dateUnix, natural : reqDate}));
+  }
 })
 
 app.listen(8080, function () {
